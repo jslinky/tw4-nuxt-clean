7bcbfc22c56e27a7644fe87b3c2b5185c2b1fd0b
Justin Small <justinsmall@mac.com>
2024-05-03 23:03:09 +0100

    nuxt config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ import * as path from 'path';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  extends: ['../purity-components'],
+  // css: ['~/assets/css/main.css', '../purity-styles/assets/test.css'],
+  css: ['../purity-styles/assets/css/main.css'],
+  extends: ['../purity-components', '../purity-styles'],
   vite: {
     plugins: [tailwindcss()]
 
