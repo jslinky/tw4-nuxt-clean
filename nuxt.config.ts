86979b52ba735c3f03c4980379903faad74c941f
Justin Small <justinsmall@mac.com>
2024-05-10 10:54:58 +0100

    amended nav to use prismic custom types. created useSettings composable for accessing new site settings custom type

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,25 @@
 import tailwindcss from '@tailwindcss/vite'
-import * as path from 'path';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   // css: ['~/assets/css/main.css', '../purity-styles/assets/test.css'],
   css: ['../purity-styles/assets/css/main.css'],
+
   extends: ['../purity-components', '../purity-styles'],
+
   vite: {
     plugins: [tailwindcss()]
 
   },
+
   vue: {
     propsDestructure: true
+  },
+
+  modules: ['@nuxtjs/prismic'],
+
+  prismic: {
+    endpoint: 'pixelpurity-clean',
   },
 })
